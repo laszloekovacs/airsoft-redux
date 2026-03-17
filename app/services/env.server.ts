9e9b9dcb4a9911fb57bf7673fba389ae374704e2
laszloekovacs <laszloekovacs@gmail.com>
2026-03-17 12:30:39 +0100

    feat: add better authentication configuration to environment schema

--- app/services/env.server.ts
+++ app/services/env.server.ts
@@ -1,12 +1,16 @@
 import { z } from "zod"
 
 const defaultConnectionString = "postgres://postgres:5435@localhost/airsoft"
+const defaultAuthSecret = "dummy_secret_dummy_secret_dummy_secret"
+const defaultAuthUrl = "http://localhost:3000"
 
 const envSchema = z.object({
 	NODE_ENV: z
 		.enum(["developement", "test", "production"])
 		.default("developement"),
 	DATABASE_URL: z.string().default(defaultConnectionString),
+	BETTER_AUTH_SECRET: z.string().default(defaultAuthSecret),
+	BETTER_AUTH_URL: z.string().default(defaultAuthUrl),
 })
 
 const _env = envSchema.parse(process.env)
