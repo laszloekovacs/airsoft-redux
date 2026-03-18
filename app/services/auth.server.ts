d8155e2ee306112c90dfed5141b90d216f452a87
laszloekovacs <laszloekovacs@gmail.com>
2026-03-18 11:13:19 +0100

    fix: added schema to auth

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -1,11 +1,19 @@
 import { betterAuth } from "better-auth"
 import { drizzleAdapter } from "better-auth/adapters/drizzle"
 import { username } from "better-auth/plugins"
+import * as authSchema from "~/schema/auth-schema"
+import * as schema from "~/schema/schema"
 import { db } from "~/services/drizzle.server"
+import { env } from "./env.server"
 
 export const auth = betterAuth({
+	baseURL: env.BETTER_AUTH_URL,
 	database: drizzleAdapter(db, {
 		provider: "pg",
+		schema: {
+			...schema,
+			...authSchema,
+		},
 	}),
 
 	emailAndPassword: {
