ee3517f9c08bc2c93598a7c1ac6b629ff4f3e137
laszlo kovacs <laszloekovacs@gmail.com>
2026-03-18 20:33:05 +0100

    feat: account page with basic info

---
+++ app/routes/account.tsx
@@ -0,0 +1,27 @@
+import { redirect } from "react-router"
+import { auth } from "~/services/auth.server"
+import type { Route } from "./+types/account"
+
+export async function loader({ request }: Route.LoaderArgs) {
+    const data = await auth.api.getSession(request)
+
+    if (!data) {
+        throw redirect("/session/login")
+    }
+
+    const { session, user } = data
+    return { session, user }
+}
+
+export default function AccountPage({ loaderData }: Route.ComponentProps) {
+    const { session, user } = loaderData
+
+    return (
+        <div>
+            <h1>Felhasznalo fiok</h1>
+
+            <p>{user.email}</p>
+            <p>{user.name}</p>
+        </div>
+    )
+}
