f12135708e8f1a6af1359d13baf9799591c15e11
laszlo kovacs <laszloekovacs@gmail.com>
2026-03-21 17:44:37 +0100

    feat: add application form to event details page with validation

--- app/routes/_app.event.$id.tsx
+++ app/routes/_app.event.$id.tsx
@@ -1,4 +1,13 @@
+import {
+	getCollectionProps,
+	getFormProps,
+	getInputProps,
+	useForm,
+} from "@conform-to/react"
+import { getZodConstraint, parseWithZod } from "@conform-to/zod/v4"
 import { eq } from "drizzle-orm"
+import { useFetcher } from "react-router"
+import z from "zod"
 import { EventTable } from "~/schema/schema"
 import { db } from "~/services/drizzle.server"
 import type { Route } from "./+types/_app.event.$id"
@@ -23,6 +32,49 @@ export default function EventDetailsPage({ loaderData }: Route.ComponentProps) {
 		<div>
 			<h1>Event</h1>
 			<pre>{JSON.stringify(event)}</pre>
+
+			<div>
+				<ApplicationForm />
+			</div>
 		</div>
 	)
+}
+
+const schema = z.object({
+	message: z.string(),
+	intent: z.enum(["apply"]).default("apply"),
+})
+
+// props should be event id and userid
+
+const ApplicationForm = () => {
+	const fetcher = useFetcher()
+	const lastResult = fetcher.data
+
+	const [form, fields] = useForm({
+		lastResult,
+		constraint: getZodConstraint(schema),
+		shouldRevalidate: "onBlur",
+		shouldValidate: "onBlur",
+		onValidate({ formData }) {
+			return parseWithZod(formData, {
+				schema,
+			})
+		},
+	})
+
+	return (
+		<div>
+			<fetcher.Form method="post" {...getFormProps(form)}>
+				<label htmlFor={fields.message.id}>uzenet</label>
+				<input {...getInputProps(fields.message, { type: "text" })} />
+				<input {...getInputProps(fields.intent, { type: "hidden" })} />
+				<button type="submit">jelentkezek</button>
+			</fetcher.Form>
+		</div>
+	)
+}
+
+export async function action({ request }: Route.ActionArgs) {
+	console.log("ello")
 }
