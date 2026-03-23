288dc34b88b6f4024be5cd02ca1ac864df0d43cf
laszlo kovacs <laszloekovacs@gmail.com>
2026-03-23 20:18:21 +0100

    feat: hono server integration

--- app/server.ts
+++ app/server.ts
@@ -1,19 +1,21 @@
 // server.ts
 
-import { serveStatic } from '@hono/node-server/serve-static'
-import { Hono } from 'hono'
-import { createHonoServer } from 'react-router-hono-server/node'
+//import { serveStatic } from "@hono/node-server/serve-static"
+import { Hono } from "hono"
+import { createHonoServer } from "react-router-hono-server/bun"
 
 const app = new Hono()
 
-// Public assets
-app.use('/public/*', serveStatic({ root: './public' }))
+app.get("/health", (c) =>
+	c.json({
+		status: "ok",
+	}),
+)
+// Public assets (these should usually come early)
 
-// Let react-router-hono-server handle everything else (client assets + SSR + SPA fallback)
-export default createHonoServer({
-  // you can pass your own hono instance if you want to add api routes before
-  app,
+//app.use('/public/*', serveStatic({ root: './public' }))
 
-  // optional: customize static folders
-  // serveStaticOptions: { ... }
-})
+// First, create the React Router server (adds asset serving + SSR catch-all `*`)
+const server = await createHonoServer({ app })
+
+export default server
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
+import {reactRouterHonoServer} from "react-router-hono-server/dev"
 import { defineConfig } from "vite"
-//import {reactRouterHonoServer} from "react-router-hono-server/dev"
-//import tsconfigPaths from "vite-tsconfig-paths"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-	plugins: [tailwindcss(), reactRouter()],
+	plugins: [tsconfigPaths(), tailwindcss(), reactRouterHonoServer({runtime:"bun"}), reactRouter()],
 	server: {
 		port: 3000,
 	},
