8d22b7d2823acd3b9aae23f2cab1ecb056663452
laszlo kovacs <laszloekovacs@gmail.com>
2026-03-24 17:37:40 +0100

    refactor: profile page

--- app/routes/profile.$username.tsx
+++ app/routes/profile.$username.tsx
@@ -1,31 +1,35 @@
 import { eq } from "drizzle-orm"
+import expectOne from "~/functions/expectone"
+import requireSession from "~/functions/requiresession"
 import { user } from "~/schema/auth-schema"
 import { db } from "~/services/drizzle.server"
 import type { Route } from "./+types/profile.$username"
 
-export async function loader({ params }: Route.LoaderArgs) {
-    // todo: only people with account should be able to see users
+export async function loader({ params, request }: Route.LoaderArgs) {
+	await requireSession(request)
 
-    const queryResult = await db
-        .select()
-        .from(user)
-        .where(eq(user.username, params.username))
+	const queryResult = await db
+		.select()
+		.from(user)
+		.where(eq(user.username, params.username))
 
-    return queryResult
+	const profile = expectOne(queryResult)
+
+	return { profile }
 }
 
 // user public profile
 export default function UserProfilePage({ loaderData }: Route.ComponentProps) {
-    if (!loaderData) return <p>nincs ilyen profil</p>
+	if (!loaderData) return <p>nincs ilyen profil</p>
 
-    return (
-        <div>
-            <p>user profile</p>
-            {loaderData && (
-                <div>
-                    <pre>{JSON.stringify(loaderData)}</pre>
-                </div>
-            )}
-        </div>
-    )
+	return (
+		<div>
+			<p>user profile</p>
+			{loaderData && (
+				<div>
+					<pre>{JSON.stringify(loaderData)}</pre>
+				</div>
+			)}
+		</div>
+	)
 }
