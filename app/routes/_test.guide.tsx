b998484a297ada9804dfe198c53e2640caa65336
laszloekovacs <laszloekovacs@gmail.com>
2026-03-30 12:02:26 +0200

    feat: form and button styles

---
+++ app/routes/_test.guide.tsx
@@ -0,0 +1,19 @@
+export default function StyleGuide() {
+	return (
+		<div className="max-w-sm">
+			<label htmlFor="email" className="input-label">
+				Email Address
+			</label>
+			<input
+				type="email"
+				id="email"
+				className="input-field"
+				placeholder="you@example.com"
+			/>
+
+			<button type="submit" className="btn">
+				hello
+			</button>
+		</div>
+	)
+}
