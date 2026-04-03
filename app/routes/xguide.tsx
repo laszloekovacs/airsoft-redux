d5931dac876c47b84f1b5f351e7906340975e245
laszloekovacs <laszloekovacs@gmail.com>
2026-04-03 19:40:26 +0200

    feat: add cap endpoint var to env

--- app/routes/xguide.tsx
+++ app/routes/xguide.tsx
@@ -1,6 +1,14 @@
 import "@cap.js/widget"
+import { useEffect, useState } from "react"
+import { env } from "~/services/env.server"
+import type { Route } from "./+types/xguide"
 
-export default function StyleGuide() {
+export const loader = () => {
+	// get the api endpoint url from env
+	return { cap_endpoint: env.CAP_CONNECTION_STRING }
+}
+
+export default function StyleGuide({ loaderData }: Route.ComponentProps) {
 	return (
 		<div className="flex flex-col items-center w-full p-12">
 			<div className="max-w-sm panel-border p-8">
@@ -22,17 +30,29 @@ export default function StyleGuide() {
 				</button>
 			</div>
 
-			<CappedForm />
+			<CappedForm endpoint={loaderData.cap_endpoint} />
 		</div>
 	)
 }
 
-const CappedForm = () => {
+// TODO: lift up state from widget with callbacks
+const CappedForm = ({ endpoint }: { endpoint: string }) => {
+	const [isMounted, setMounted] = useState(false)
+
+	useEffect(() => {
+		setMounted(true)
+	}, [])
+
+	if (!isMounted) {
+		return <div>loading...</div>
+	}
+
+	// submit form to action, the token will be added to the form (?cap_token=...)
 	return (
 		<div>
 			<form>
 				<cap-widget
-					data-cap-api-endpoint="endpoint:3003/sitecode"
+					data-cap-api-endpoint={endpoint}
 					onsolve={(e) => console.log("token:", e.detail.token)}
 					onprogress={(e) => console.log(e.detail.progress)}
 					onerror={(e) => console.error(e.detail.message)}
