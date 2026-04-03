0e0a75c6ae051924522b7ffbe360ffaac72a40f8
laszloekovacs <laszloekovacs@gmail.com>
2026-04-03 16:15:38 +0200

    fix: specify TaskData type for worker and add completion logging

--- app/workers/stats.server.ts
+++ app/workers/stats.server.ts
@@ -7,7 +7,7 @@ type TaskData = {
 // both should be embedded: true
 // the name of the queue and the worker establises the connection between the 2
 const queue = new Queue<TaskData>("test", { embedded: true })
-const worker = new Worker(
+const worker = new Worker<TaskData>(
 	"test",
 	async (job) => {
 		console.log("processing:", job.data)
@@ -16,6 +16,10 @@ const worker = new Worker(
 	},
 	{ embedded: true },
 )
+
+worker.on("completed", (job, result) => {
+	console.log(`Job ${job.id} completed: ${result}`)
+})
 
 // call this in a loader or action
 export const scheduleJob = async () => {
