a081309a4f1f44d2c8a0b7c51255dfe5aeec47d3
laszloekovacs <laszloekovacs@gmail.com>
2026-04-03 19:12:41 +0200

    cap integration

--- app/routes/xguide.tsx
+++ app/routes/xguide.tsx
@@ -1,3 +1,5 @@
+import "@cap.js/widget"
+
 export default function StyleGuide() {
 	return (
 		<div className="flex flex-col items-center w-full p-12">
@@ -19,6 +21,24 @@ export default function StyleGuide() {
 					hello
 				</button>
 			</div>
+
+			<CappedForm />
+		</div>
+	)
+}
+
+const CappedForm = () => {
+	return (
+		<div>
+			<form>
+				<cap-widget
+					data-cap-api-endpoint="endpoint:3003/sitecode"
+					onsolve={(e) => console.log("token:", e.detail.token)}
+					onprogress={(e) => console.log(e.detail.progress)}
+					onerror={(e) => console.error(e.detail.message)}
+				/>
+				<button type="submit">Submit</button>
+			</form>
 		</div>
 	)
 }
