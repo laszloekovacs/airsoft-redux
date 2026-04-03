d5c4cd44ddea7eddb57b289701bff67a8ae9f5c3
laszloekovacs <laszloekovacs@gmail.com>
2026-04-03 15:31:16 +0200

    add bunqueue framework

---
+++ app/workers/stats.server.ts
@@ -0,0 +1,20 @@
+import { Queue, Worker } from "bunqueue/client"
+
+console.log("stats is running")
+// both should be embedded: true
+// the name of the queue and the worker establises the connection between the 2
+const queue = new Queue("test", { embedded: true })
+const worker = new Worker(
+	"test",
+	async (job) => {
+		console.log("processing:", job.data)
+
+		return { success: true }
+	},
+	{ embedded: true },
+)
+
+// call this in a loader or action
+export const scheduleJob = async () => {
+	await queue.add("working", { message: "bunqueue is working" })
+}
