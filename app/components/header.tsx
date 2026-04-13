4ad7af89cc5f96f2228add98419c558d12063f70
laszlo kovacs <laszloekovacs@gmail.com>
2026-04-13 14:18:00 +0200

    chore: header styling

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router"
 
 export const PageHeader = () => {
 	return (
-		<div className="flex flex-row py-2 text-2xl">
+		<div className="text-2xl font-extrabold hover:text-primary transition-colors">
 			<Link to="/">Airsoft</Link>
 		</div>
 	)
