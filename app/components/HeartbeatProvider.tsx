7a043f6618f3dc5d6bcc64497a6887b7cfd63e7f
laszloekovacs <laszloekovacs@gmail.com>
2026-04-17 10:43:58 +0200

    refactor: removed shared worker, pinging simplified

--- app/components/HeartbeatProvider.tsx
+++ app/components/HeartbeatProvider.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useRef } from "react"
 
-// creates a wrapper that starts the heartbeat shared worker
+const DELAY = 10_000
+const ENDPOINT_URL = "/api/presence/"
+
+// send a request to the heartbeat endpoint in an interval
 export const HeartbeatProvider = ({
 	userId,
 	children,
@@ -8,24 +11,21 @@ export const HeartbeatProvider = ({
 	userId: string | null
 	children: React.ReactNode
 }) => {
-	const workerRef = useRef<SharedWorker>(null)
+	const intervalRef = useRef<number | null>(null)
 
-	// could be a custom hook, however, we wont be reusing it
 	useEffect(() => {
 		if (userId != null) {
-			workerRef.current = new SharedWorker("/hearbeat-worker.js")
+			intervalRef.current = window.setInterval(() => {
+				fetch(ENDPOINT_URL + userId, {
+					method: "post",
+				})
+			}, DELAY)
+		}
 
-			// start the worker and pass the user id to it
-			workerRef.current.port.start()
-			workerRef.current.port.postMessage({ userId })
-
-			// questionable choice, i think garbage collector will deal with it anyway
-			window.addEventListener("beforeunload", () => {
-				workerRef.current?.port.close()
-			})
-
-			return () => {
-				workerRef.current?.port.close()
+		return () => {
+			if (intervalRef.current) {
+				clearInterval(intervalRef.current)
+				intervalRef.current = null
 			}
 		}
 	}, [userId])
